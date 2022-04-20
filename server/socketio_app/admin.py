ddbd8b19165e08bb7ea4d0ac02494fe39e898e52
jake-schmm <67937281+jake-schmm@users.noreply.github.com>
2022-04-20 12:22:55 -0400

    Update admin.py

--- server/socketio_app/admin.py
+++ server/socketio_app/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
 
-# Register your models here.
->>>>>>> aac961c9dde0f2b1c1be0a2dd8303237ef1a7760
