d11b20ddf90a94e2314cdbf0fa8a7149f73988a5
jake-schmm <67937281+jake-schmm@users.noreply.github.com>
2022-04-20 12:23:14 -0400

    Update apps.py

--- server/socketio_app/apps.py
+++ server/socketio_app/apps.py
@@ -4,11 +4,4 @@ from django.apps import AppConfig
 class SocketioAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "socketio_app"
-=======
-from django.apps import AppConfig
 
-
-class SocketioAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "socketio_app"
->>>>>>> aac961c9dde0f2b1c1be0a2dd8303237ef1a7760
