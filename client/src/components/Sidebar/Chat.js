8c33a5d073a6ca7d073924c1ee2d245457a0cce3
jake-schmm <67937281+jake-schmm@users.noreply.github.com>
2022-04-26 11:20:19 -0400

    Update Chat.js

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { Box } from '@material-ui/core';
 import { BadgeAvatar, ChatContent } from '../Sidebar';
 import { makeStyles } from '@material-ui/core/styles';
@@ -21,9 +21,12 @@ const Chat = ({ conversation, setActiveChat, passChildData, activeConvo }) => {
   const classes = useStyles();
   const { otherUser } = conversation;
 
+  const [count, setCount] = useState(0);
+  const latestMessageText = conversation.id && conversation.latestMessageText;
+
   const handleClick = async (conversation) => {
     await setActiveChat(conversation.otherUser.username);
-    
+    setCount(0);
     passDataToSidebar(conversation.id);
   };
 
@@ -31,6 +34,35 @@ const Chat = ({ conversation, setActiveChat, passChildData, activeConvo }) => {
     passChildData(data);
   }
 
+  
+  
+  var numUnreadMessages = 0;
+  conversation.messages.forEach((message) => {
+    if(message.senderId == otherUser.id) {
+      numUnreadMessages++; 
+    }
+  });
+
+  useEffect(() => {
+    setCount(numUnreadMessages);
+  }, [])
+
+
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) { 
+       firstUpdate.current = false;
+    }
+    else {
+      // if conversation is not currently open and a message is sent by other user, add to count
+      if(activeConvo.current != conversation.id) {
+        if(conversation.messages[conversation.messages.length-1].senderId == otherUser.id) {
+          setCount(count + 1);
+      }
+        }
+    }
+  }, [latestMessageText])
+  
   return (
     <Box onClick={() => handleClick(conversation)} className={classes.root}>
       <BadgeAvatar
@@ -39,7 +71,7 @@ const Chat = ({ conversation, setActiveChat, passChildData, activeConvo }) => {
         online={otherUser.online}
         sidebar={true}
       />
-      <ChatContent conversation={conversation} activeConvo = {activeConvo}/>
+      <ChatContent conversation={conversation} activeConvo = {activeConvo} count={count}/>
     </Box>
   );
 };
