2d5e91773e1afbbd390a0c407dcb766b5c92bc25
jake-schmm <jake.schmm@gmail.com>
2022-04-27 12:43:04 -0400

    Changed Home, Messages, ActiveChat

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
 import { Input, Header, Messages } from './index';
@@ -27,16 +27,24 @@ const ActiveChat = ({
 }) => {
   const classes = useStyles();
 
+  
+  
+ 
+
+  
+ 
+
   const conversation = conversations
     ? conversations.find(
         (conversation) => conversation.otherUser.username === activeConversation
       )
     : {};
 
-
   const isConversation = (obj) => {
     return obj !== {} && obj !== undefined;
   };
+ 
+ 
 
   return (
     <Box className={classes.root}>
