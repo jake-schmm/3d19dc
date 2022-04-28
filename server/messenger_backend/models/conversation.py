c2e8cb06cca9b7ef657a8ffc520c7ce8ae40c10b
jake-schmm <67937281+jake-schmm@users.noreply.github.com>
2022-04-28 13:01:58 -0400

    Update conversation.py

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -8,29 +8,23 @@ from functools import reduce
 
 class Conversation(utils.CustomModel):
 
-    # user1 = models.ForeignKey(
-    #    User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    #)
-    # user2 = models.ForeignKey(
-    #    User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    # )
+    user = models.ManyToManyField(User, on_delete=models.CASCADE)
 
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
+    # find conversation given multiple user Ids
     def find_conversation(userIds):
         # return conversation or None if it doesn't exist
+        users = []
+        for userId in userIds:
+            users.append(User.get_by_id(userId))
         try:
-            # user_ids would be a list in string format - comma-separated 
-            query = reduce(operator.and_, (Q(user_ids__contains = id) for id in userIds))
-            return Conversation.objects.filter(query)
-            
-            
-            # return Conversation.objects.get(
-            #    (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-            #    (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            # )
-            
+                convos = []
+                for user in users:
+                    convosThatIncludeUser = user.conversation_set.all()
+                    convos.append(convosThatIncludeUser)
+                intersect = set.intersection(*map(set,convos))
+                return intersect
         except Conversation.DoesNotExist:
             return None
